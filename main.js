bb15bf986f44f5b031ab6ed59275aa1ddbb3a407
Learner8907 <72011994+Learner8907@users.noreply.github.com>
2021-06-26 19:03:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,29 @@ function setup(){
 canvas = createCanvas(300,300)
 canvas.center()
 video= createCapture(VIDEO);
+video.size(300,300);
 video.hide();
 
+PoseNet = ml5.poseNet(video, modelLoaded);
+PoseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized')
+}
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+
+    }
+}
+function take_snapshot(){
+    save('myFilterImg.png');
 }
 function draw(){
     image(video, 0, 0, 300, 300);
 }
+
